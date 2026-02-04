c7ec1a672e149f4fde8d368ec3dec24fc91453c7
Logan-Vargas <156100152+Logan-Vargas@users.noreply.github.com>
2026-02-04 20:12:57 +0000

    finished

--- Jokes2.py
+++ Jokes2.py
@@ -1,10 +1,25 @@
-# Joke Creation Tool (refactoring the bad performance task) - About 80% finished
-# By Logan Vargas and Antonio Williams
+# Joke Creation Tool (refactoring the bad performance task) 
+# By Antonio Williams and Logan Vargas
 
 system_on = True
 joke_tag = []
 joke_punch = []
 
+# Final Phase - User rates the game and gives recommendation
+def final_phase():
+    rate = int(input("Please rate our game 1-10! "))
+final_score = int(rate * 10)
+print(str(final_score) + " percent satisfaction rate")
+
+# Recommendation Function
+def recommendation():
+    friend = input("Would you recommend this game to a friend? ")
+if friend == "yes" or friend == "maybe":
+        print("Thanks, we appreciate it. ")
+else:
+    print("Sorry you did not enjoy it. ")
+
+# Knock Knock Joke Function
 def commence_the_knock_knock(joke_value):
     input("Knock Knock ")
     input(joke_tag[joke_value])
@@ -12,10 +27,10 @@ def commence_the_knock_knock(joke_value):
     want_joke = input("Do you want to hear another joke or are you finished? Type 'end' to proceed to the next phase. ")
     return want_joke
 
-
+# Joke Creation Phase
 # For as long as you want to make new jokes, then you can make as many as you want! The program will only stop looping after the user types in "end."
 while system_on == True:
-
+# Here, the user is prompted to create a joke by naming it and providing a punchline.
     init_input = str(input("Welcome to the joke creation tool! Start by naming the joke that you want to make, or type 'end' to proceed to the next phase. "))
     if init_input.lower() == "end":
         break
@@ -26,7 +41,7 @@ while system_on == True:
         print(joke_tag)
         print(joke_punch)
         print("Great joke!")
-
+# Joke Telling Phase
 # Now, you can bring another person over to laugh at the jokes you've created! The program will display all of the jokes for you, and the user can select which one they want.
 print("Your friend made a slew of jokes for you to laugh at!")
 while system_on:
@@ -34,7 +49,7 @@ while system_on:
 # Here, the program will display the names of the jokes, and will move on if none are created! The user can select which joke they want.
     print("The names of the jokes are as follows:")
     print(joke_tag)
-    friend_input = str(input("Which joke do you want? Type 'end' to proceed to the next phase. "))
+    friend_input = str(input("Which joke do you want? Type 'end' to proceed to the next phase. ")) # User selects which joke they want to hear.
     if friend_input.lower() == "end":
         break
     else:
@@ -51,14 +66,6 @@ while system_on:
             print("No match, try again!")
         if heard_joke == 2:
             break
-
-def final_phase():
-    rate = int(input("Please rate our game 1-10! "))
-final_score = int(rate * 10)
-print(str(final_score) + " percent satisfaction rate")
-friend = input("Would you recommend this game to a friend? ")
-if friend == "yes" or friend == "maybe":
-        print("Thanks, we appreciate it. ")
-else:
-    print("Sorry you did not enjoy it. ")
-    
+# Final Phase - User rates the game and gives recommendation
+final_phase()
+recommendation()
