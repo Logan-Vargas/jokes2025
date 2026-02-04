47a1c73e820d2c28a114d4bdb3850c7d81ac246d
Logan-Vargas <156100152+Logan-Vargas@users.noreply.github.com>
2026-02-04 20:06:36 +0000

    finished?

--- Jokes2.py
+++ Jokes2.py
@@ -52,3 +52,13 @@ while system_on:
         if heard_joke == 2:
             break
 
+def final_phase():
+    rate = int(input("Please rate our game 1-10! "))
+final_score = int(rate * 10)
+print(str(final_score) + " percent satisfaction rate")
+friend = input("Would you recommend this game to a friend? ")
+if friend == "yes" or friend == "maybe":
+        print("Thanks, we appreciate it. ")
+else:
+    print("Sorry you did not enjoy it. ")
+    
