7f63668bb2a9c081213dbf2e0a6c32d2842bb7de
Logan-Vargas <156100152+Logan-Vargas@users.noreply.github.com>
2026-02-04 20:01:32 +0000

    added more

--- Jokes2.py
+++ Jokes2.py
@@ -1,23 +1,23 @@
-# make this performance task ready for submission
-# To give the user a fun experience hearing knock knock jokes
-#The purpose is going to be able to create jokes
+# Joke Creation Tool (refactoring the bad performance task) - About 80% finished
+# By Logan Vargas and Antonio Williams
 
-def commence_the_knock_knock(joke_value):
-    input("Knock Knock")
-    input(joke_tag[joke_value])
-    print(joke_punch[joke_value])
-    want_joke = input("Do you want to hear another joke or are you finished? ")
-    return want_joke
-
-
-system_on = 1
+system_on = True
 joke_tag = []
 joke_punch = []
 
-while system_on == 1:
+def commence_the_knock_knock(joke_value):
+    input("Knock Knock ")
+    input(joke_tag[joke_value])
+    print(joke_punch[joke_value])
+    want_joke = input("Do you want to hear another joke or are you finished? Type 'end' to proceed to the next phase. ")
+    return want_joke
+
+
+# For as long as you want to make new jokes, then you can make as many as you want! The program will only stop looping after the user types in "end."
+while system_on == True:
 
     init_input = str(input("Welcome to the joke creation tool! Start by naming the joke that you want to make, or type 'end' to proceed to the next phase. "))
-    if init_input.lower() == 'end':
+    if init_input.lower() == "end":
         break
     else:
         joke_tag.append(init_input)
@@ -25,21 +25,30 @@ while system_on == 1:
         joke_punch.append(punch_input)
         print(joke_tag)
         print(joke_punch)
-        print('Cool!')
+        print("Great joke!")
 
-while system_on == 1:
-    print("Your friend made a slew of jokes for you to laugh at ")
-    print('The names of the jokes are as follows:')
+# Now, you can bring another person over to laugh at the jokes you've created! The program will display all of the jokes for you, and the user can select which one they want.
+print("Your friend made a slew of jokes for you to laugh at!")
+while system_on:
+
+# Here, the program will display the names of the jokes, and will move on if none are created! The user can select which joke they want.
+    print("The names of the jokes are as follows:")
     print(joke_tag)
-    friend_input = str(input("Which joke do you want? Type 'end' to proceed to the next phase: "))
-    if friend_input.lower == 'end':
+    friend_input = str(input("Which joke do you want? Type 'end' to proceed to the next phase. "))
+    if friend_input.lower() == "end":
         break
     else:
         temp1 = -1
+        heard_joke = 0
         for x in joke_tag:
             temp1 += 1
-            if friend_input.lower() ==  x.lower():
-                commence_the_knock_knock(temp1)
+            if friend_input.lower() == x.lower():
+                end_early = commence_the_knock_knock(temp1)
                 heard_joke = 1
+                if end_early == "end":
+                    heard_joke = 2
         if heard_joke == 0:
-                print("No match, try again!")
+            print("No match, try again!")
+        if heard_joke == 2:
+            break
+
